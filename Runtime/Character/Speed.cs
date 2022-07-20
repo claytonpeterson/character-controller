fcbc54d93559264c140f5a09c26b5960ce860e91
Clay Peterson <claytonpetersonmail@gmail.com>
2022-07-20 00:42:08 -0400

    Remove MoveSpeed.cs

--- Runtime/Character/Speed.cs
+++ Runtime/Character/Speed.cs
@@ -1,22 +1,49 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace CharacterMovement
 {
     [System.Serializable]
     public class Speed
     {
-        [SerializeField]
-        private MoveSpeed crouch;
+        [System.Serializable]
+        private class MoveSpeed
+        {
+            [SerializeField] private float minSpeed;
+            [SerializeField] private float maxSpeed;
+            [SerializeField] private float acceleration;
+            [SerializeField] private float deceleration;
 
-        [SerializeField]
-        private MoveSpeed walk;
+            private float lerp;
 
-        [SerializeField]
-        private MoveSpeed run;
+            public float Speed()
+            {
+                return Mathf.Lerp(minSpeed, maxSpeed, lerp);
+            }
 
-        [SerializeField]
-        private MoveSpeed[] speeds;
+            public float Percent()
+            {
+                return lerp;
+            }
+
+            public void Accelerate()
+            {
+                if (lerp < 1)
+                    lerp += Time.deltaTime / acceleration;
+            }
+
+            public void Decelerate()
+            {
+                if (lerp > 0)
+                    lerp -= Time.deltaTime / deceleration;
+            }
+
+            public void Reset()
+            {
+                lerp = 0;
+            }
+        }
+
+        [SerializeField] private MoveSpeed[] speeds;
 
         private int speedIndex = 0;
 
